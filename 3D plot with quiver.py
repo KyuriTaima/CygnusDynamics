83edb2b4ec6992a64c551476fb37c85fb4632e52
Sulwen de la Croix <sulwen.delacroix@gmail.com>
2026-06-14 15:54:35 +0200

    Overleaf Connect auto-sync [2026-06-14 13:54:35]

--- 3D plot with quiver.py
+++ 3D plot with quiver.py
@@ -24,7 +24,7 @@ y_gal = distances_pc * np.sin(l_rad) * np.cos(b_rad)
 z_gal = distances_pc * np.sin(b_rad)
 
 # On trouve l'index de Group E
-index_origin = list(object_names).index("Group E") 
+index_origin = list(object_names).index("W75N") 
 x_origin, y_origin, z_origin = x_gal[index_origin], y_gal[index_origin], z_gal[index_origin]
 beta_angle = np.arctan2(y_origin, x_origin)
 
