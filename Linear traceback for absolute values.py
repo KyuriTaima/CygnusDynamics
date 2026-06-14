70a5cd297f5643c2dcf6f3ab43293753e34852f2
Sulwen de la Croix <sulwen.delacroix@gmail.com>
2026-06-14 17:55:10 +0200

    Overleaf Connect auto-sync [2026-06-14 15:55:09]

--- Linear traceback for absolute values.py
+++ Linear traceback for absolute values.py
@@ -120,6 +120,7 @@ for t in target_times:
     plt.xlabel('X (pc)')
     plt.ylabel('Y (pc)')
     plt.title(f'Positions of Cygnus X Objects in the Galactic plane at -{t:.1f} Myr')
+    plt.gca().set_aspect('equal', adjustable='box')
     plt.xlim(-100, 1000)
     plt.ylim(500, -100)
     plt.legend()
