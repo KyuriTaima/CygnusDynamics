6affb06412bd64038d7cacf33697688714fdb1e2
Sulwen de la Croix <sulwen.delacroix@gmail.com>
2026-06-15 16:54:33 +0200

    Overleaf Connect auto-sync [2026-06-15 14:54:33]

--- Graph_projection_plan_propre_avec_background.py
+++ Graph_projection_plan_propre_avec_background.py
@@ -54,6 +54,7 @@ arrow_colors = ['blue' if 'Group' in str(nom) else 'black' for nom in names]
 # Superposition des vecteurs vitesse (zorder=3 pour être au-dessus du fond)
 plt.quiver(l_deg, b_deg, v_l_kms, v_b_kms, angles='xy', scale_units='xy', scale=15, color=arrow_colors, width = 0.003, headwidth = 3.5, headlength = 4)
 
+# Use diffente colors, green for groups B,E and C; red for groups A,D and F; blue for the rest
 plt.scatter(l_deg, b_deg, color='black', marker='o', s=10, label='Objects')
 
 # Boucle pour ajouter les noms des objets
