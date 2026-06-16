10d1f8b40affcba42f719b73dee36988fd2f1e93
Sulwen de la Croix <sulwen.delacroix@gmail.com>
2026-06-16 13:38:22 +0200

    Overleaf Connect auto-sync [2026-06-16 11:38:22]

--- Linear traceback for absolute values.py
+++ Linear traceback for absolute values.py
@@ -131,8 +131,8 @@ for t in target_times:
 
     plt.annotate(
         'To Galactic Center', 
-        xy=(40, 300),             
-        xytext=(40, 220),         
+        xy=(40, 240),             
+        xytext=(40, 190),         
         arrowprops=dict(facecolor='black', width=2, headwidth=8, alpha=0.6, shrink=0.05),
         fontsize=10, fontweight='bold', color='black',
         ha='center', va='top', zorder=4,
