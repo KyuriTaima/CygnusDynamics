049c75a9c0e3717d5e1faa9ae1ea3c0edaba734e
Sulwen de la Croix <sulwen.delacroix@gmail.com>
2026-06-16 11:28:10 +0200

    Overleaf Connect auto-sync [2026-06-16 09:28:10]

--- Plot Galactic Plane.py
+++ Plot Galactic Plane.py
@@ -181,9 +181,9 @@ for i in range(len(names)):
     couleur_texte = 'darkred' if 'Group' in str(names[i]) else 'teal'
     # Adjust the text position for IRAS to avoid overlap
     if names[i] == "IRAS20290+4052":
-        xytext = (-50, 6)
-    elif names[i] == "Group A":
-        xytext = (-10, 6)
+        xytext = (-50, -50)
+    elif names[i] == "Group E":
+        xytext = (-50, 10)
     else:
         xytext = (5, 5)
 # annotate if the object is not W75N or Group E or Group D
