aca6c075669dbf31965d9b5dfec1a2af6c231782
Sulwen de la Croix <sulwen.delacroix@gmail.com>
2026-06-16 11:12:39 +0200

    Overleaf Connect auto-sync [2026-06-16 09:12:39]

--- Plot Galactic Plane.py
+++ Plot Galactic Plane.py
@@ -165,10 +165,10 @@ W_pec_kms = objects_data['W_pec_kms'].values
 
 # Velocity vectors
 Q = plt.quiver(X_local, Z_gal, V_pec_kms, W_pec_kms, 
-           angles='xy', scale_units='xy', scale=0.2, 
+           angles='xz', scale_units='xz', scale=0.2, 
            color=arrow_colors, width=0.003, headwidth=3.5, headlength=4, zorder=3)
 
-plt.quiverkey(Q, X=0.70, Y=0.97, U=10, 
+plt.quiverkey(Q, X=0.70, Z=0.97, U=10, 
               label='10 km/s', labelpos='E', 
               coordinates='axes', fontproperties={'weight': 'bold', 'size': 10}, 
               color='black')
